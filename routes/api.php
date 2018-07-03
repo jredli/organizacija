4664302619acb7a925e45f190c0a23f72cc5ded8
Redli <jovan@concept7.io>
2018-07-03 11:11:29 +0200

    JWT setup

--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,6 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::post('signup', 'AuthController@signup');
 
 });
